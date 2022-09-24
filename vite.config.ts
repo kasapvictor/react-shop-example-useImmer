adbb4cf9225d769b9b504e378d6cabb9ac95852c
Victor Kasap <kasap.victor@gmail.com>
2022-09-24 20:14:14 +0300

    upd: Updated vite config - base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const isProdMode = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     port: 4000,
     host: '0.0.0.0',
